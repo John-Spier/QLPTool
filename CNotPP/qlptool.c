16b4187c2ca56da61a2794f797f031a7631f797c
John-Spier <102952652+John-Spier@users.noreply.github.com>
2022-05-20 01:12:13 -0400

    Now compiles on older standards and compiles (VC98 tested)

--- CNotPP/qlptool.c
+++ CNotPP/qlptool.c
@@ -22,38 +22,42 @@ int main(int argc, char* argv[]);
 
 
 int splitqlp(int c, char* v[]) {
-
-
+	int i;
+	char header[4];
+	long int filenum = 0;
+	QLPFILE* filelist;
+	QLPFILE* readtmp;
+	int buffer_left = 0;
+	char* buffer;
+	FILE* outqlp;
 	FILE* inqlp = fopen(v[2], "rb+");
 	if (!inqlp) {
 		perror("Can't open QLP");
 		return 1;
 	}
-	char header[4];
-	long int filenum = 0;
+
 	fread(header, 1, 4, inqlp);
 	if (memcmp(magic, header, 4)) {
 		printf("Wrong QLP Header!\n");
 	}
 	fread(&filenum, addr_size, 1, inqlp);
 	//printf("Files in QLP: %i\n", filenum);
-	QLPFILE* filelist = calloc(filenum, sizeof(QLPFILE));
+	filelist = calloc(filenum, sizeof(QLPFILE));
 	if (!filelist) {
 		printf("Malloc error!\n");
 	}
-	QLPFILE* readtmp = filelist;
-	for (int i = 0; i < filenum; i++) {
+	readtmp = filelist;
+	for (i = 0; i < filenum; i++) {
 		fread(readtmp, sizeof(QLPFILE), 1, inqlp);
 		readtmp++;
 	}
 	readtmp = filelist;
-	int buffer_left = 0;
-	char* buffer;
-	for (int i = 0; i < filenum; i++) {
+
+	for (i = 0; i < filenum; i++) {
 		buffer_left = readtmp->size;
 		fseek(inqlp, readtmp->addr * addr_size, SEEK_SET);
 		//printf("Saving file #%i %s (%i bytes)...\n", i + 1, readtmp->name, readtmp->size);
-		FILE* outqlp = fopen(readtmp->name, "wb+");
+		outqlp = fopen(readtmp->name, "wb+");
 		if (!outqlp) {
 			perror("Can't open output file");
 			return 1;
@@ -83,12 +87,14 @@ int splitqlp(int c, char* v[]) {
 }
 
 int makeqlp(int c, char* v[]) {
-	int oldsize = 0;
+	int oldsize, padbytes, fnlen, i, j, buffer_left;
+	char* buffer;
+	FILE* outqlp;
 	long int filenum = c - 2;
 	QLPFILE* filelist = calloc(filenum, sizeof(QLPFILE));
 	QLPFILE* writetmp = filelist;
 	long int lastaddr = (filenum * (sizeof(QLPFILE) / addr_size)) + 2; //Header must be the same size as system pointer (4)!
-	for (int i = 2; i < c; i++) {
+	for (i = 2; i < c; i++) {
 		FILE* inqlp = fopen(v[i], "rb+");
 		if (!inqlp) {
 			perror("Couldn't open input file");
@@ -97,7 +103,7 @@ int makeqlp(int c, char* v[]) {
 		}
 		fseek(inqlp, 0, SEEK_END);
 		writetmp->size = ftell(inqlp);
-		int padbytes = addr_size - (writetmp->size % addr_size);
+		padbytes = addr_size - (writetmp->size % addr_size);
 		if (padbytes == addr_size) {
 			padbytes = 0;
 		}
@@ -106,20 +112,20 @@ int makeqlp(int c, char* v[]) {
 			oldsize = writetmp->size + padbytes;
 		}
 		else {
-			
+
 			writetmp->addr = (oldsize / addr_size) + lastaddr;
 			oldsize = writetmp->size + padbytes;
 		}
-		
+
 		lastaddr = writetmp->addr;
-		int fnlen = strlen(v[i]);
+		fnlen = strlen(v[i]);
 		if (fnlen > 15) {
-			for (int j = 0; j < 15; j++) {
+			for (j = 0; j < 15; j++) {
 				writetmp->name[j] = v[i][j + (fnlen - 15)];
 			}
 		}
 		else {
-			for (int j = 0; j < fnlen; j++) {
+			for (j = 0; j < fnlen; j++) {
 				writetmp->name[j] = v[i][j];
 			}
 		}
@@ -127,7 +133,7 @@ int makeqlp(int c, char* v[]) {
 		fclose(inqlp);
 		writetmp++;
 	}
-	FILE* outqlp = fopen(v[1], "wb+");
+	outqlp = fopen(v[1], "wb+");
 	if (!outqlp) {
 		perror("Couldn't open output QLP file");
 		return 1;
@@ -136,9 +142,8 @@ int makeqlp(int c, char* v[]) {
 	fwrite(&filenum, 1, addr_size, outqlp);
 	fwrite(filelist, filenum, sizeof(QLPFILE), outqlp);
 	writetmp = filelist;
-	int buffer_left = 0;
-	char* buffer;
-	for (int i = 2; i < c; i++) {
+	buffer_left = 0;
+	for (i = 2; i < c; i++) {
 		FILE* inqlp = fopen(v[i], "rb+");
 		if (!inqlp) {
 			perror("Couldn't open input file");
@@ -163,7 +168,7 @@ int makeqlp(int c, char* v[]) {
 			}
 			free(buffer);
 		}
-		int padbytes = addr_size - (writetmp->size % addr_size);
+		padbytes = addr_size - (writetmp->size % addr_size);
 		if (padbytes == addr_size) {
 			padbytes = 0;
 		}
@@ -177,16 +182,16 @@ int makeqlp(int c, char* v[]) {
 
 int main(int argc, char* argv[]) {
 	if (argc < 3) {
-		printf("Usage: %s [options] outfile infile [infile2] [infile3...\n",argv[0]);
+		printf("Usage: %s [options] outfile infile [infile2] [infile3...\n", argv[0]);
 		printf("Option -x extracts all resources from the first file passed as an argument.\n");
 		printf("Otherwise, QLPTool will pack all other specified files into the first file.\n");
 		return 0;
 	}
-	else if (!strcmp(argv[1] , "-x")) {
-		return splitqlp(argc,argv);
+	else if (!strcmp(argv[1], "-x")) {
+		return splitqlp(argc, argv);
 	}
 	else {
 		return makeqlp(argc, argv);
 	}
-	
+
 }
